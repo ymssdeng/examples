d51b711d96968e86cd08ef23a2ea4261e33383a8
denghui <tianma_25@163.com>
2018-03-26 11:09:44 +0800

    rabbmitmq examples

---
+++ examples/examples-rabbmitmq/src/main/java/me/forward/examples/RabbmitmqApplication.java
@@ -0,0 +1,15 @@
+package me.forward.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RabbmitmqApplication
+ */
+@SpringBootApplication
+public class RabbmitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbmitmqApplication.class, args);
+    }
+}

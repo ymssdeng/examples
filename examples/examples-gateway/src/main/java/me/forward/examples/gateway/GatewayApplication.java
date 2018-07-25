e9d3f043e646f4fe22e803355ee52a0d88640bf1
denghui <tianma_25@163.com>
2018-07-25 17:45:32 +0800

    zuul

---
+++ examples/examples-gateway/src/main/java/me/forward/examples/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package me.forward.examples.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author denghui
+ * @create 2018/7/25
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

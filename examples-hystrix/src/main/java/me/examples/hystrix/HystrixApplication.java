abf3b2c9b9b4181f886480a63d14f2a9b4e3687f
denghui <denghui@u51.com>
2018-09-26 15:34:30 +0800

    simple test

--- examples-hystrix/src/main/java/me/examples/hystrix/HystrixApplication.java
+++ examples-hystrix/src/main/java/me/examples/hystrix/HystrixApplication.java
@@ -2,6 +2,10 @@ package me.examples.hystrix;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author denghui
@@ -12,16 +16,30 @@ public class HystrixApplication {
     static class TestCommand extends HystrixCommand<String> {
 
         public TestCommand() {
-            super(HystrixCommandGroupKey.Factory.asKey("key"));
+            super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("TestCommand"))
+                .andCommandKey(HystrixCommandKey.Factory.asKey("hello"))
+                .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.defaultSetter()
+                    .withCoreSize(2)
+                    .withMaxQueueSize(2))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.defaultSetter()
+                    .withExecutionTimeoutInMilliseconds(200)
+                    .withCircuitBreakerRequestVolumeThreshold(2)
+                    .withCircuitBreakerErrorThresholdPercentage(100)));
         }
 
         @Override
         protected String run() throws Exception {
-            return "hello";
+            throw new RuntimeException("xx");
+            //return "hello";
+        }
+
+        @Override
+        protected String getFallback() {
+            return "this is fallback";
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new TestCommand().isExecutedInThread());
+        System.out.println(new TestCommand().execute());
     }
 }
